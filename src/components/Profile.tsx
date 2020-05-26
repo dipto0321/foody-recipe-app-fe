efa4ca82c5fd96a9e4131da2f0f86069a7e2862b
dipto0321 <diptokmk47@gmail.com>
2020-05-26 20:56:20 +0600

    :sparkles: Profile api first version
    - Profile ui implement
    - Add gravatar utils method for get gravatar url

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Avatar,
+  Text,
+  ButtonGroup,
+  Button,
+  Heading,
+  Divider,
+  Stack,
+  useToast,
+} from '@chakra-ui/core';
 import { ProfileProps } from '../interfaces/profile';
+import getGravatar from '../utils/gravatar';
+import { endPointPaths } from '../utils/configs';
+import serverAPI from '../apis/baseApi';
 
 const Profile = ({ accessData }: ProfileProps) => {
-  return <div>Profile</div>;
+  const initialValues = { name: '', email: '', avaterUrl: '' };
+  const [profileData, setProfileData] = useState(initialValues);
+  const toast = useToast();
+  const handleGetProfileData = async () => {
+    try {
+      const response = await serverAPI.get(endPointPaths.profilePath, {
+        headers: {
+          Authorization: `Bearer ${accessData.access}`,
+        },
+      });
+      const { data } = response;
+      setProfileData({ ...data, avaterUrl: getGravatar(data.email, 250) });
+    } catch (error) {
+      toast({
+        position: 'top',
+        title: `Opps! Something is wrong!`,
+        description: error.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleGetProfileData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Stack maxW="lg" mx="auto" mt={2}>
+      <Heading as="h1" textAlign="center">
+        PROFILE
+      </Heading>
+      <Divider />
+      <Box
+        maxW="lg"
+        mx="auto"
+        my="10px"
+        borderWidth="1px"
+        rounded="lg"
+        overflow="hidden"
+        d="flex"
+        justifyItems="center"
+        alignItems="center"
+        p={5}
+      >
+        <Avatar
+          size="2xl"
+          name={profileData.name}
+          src={profileData.avaterUrl}
+          m={5}
+        />
+        <Box>
+          <Text
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            color="#2c3e50"
+          >
+            {profileData.name}
+          </Text>
+          <Text color="#2980b9">{profileData.email}</Text>
+          <ButtonGroup spacing={2} mt={2}>
+            <Button rightIcon="edit" variantColor="yellow" variant="solid">
+              Edit
+            </Button>
+            <Button rightIcon="delete" variantColor="red" variant="solid">
+              Delete
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Profile;

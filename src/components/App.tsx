47ecf4e4af17aa2f51aea91b3f75b387b13b67cd
dipto0321 <diptokmk47@gmail.com>
2020-08-05 16:50:11 +0600

    :lipstick: Add antd
    - New react component UI is added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { Button } from 'antd';
 import Navbar from './Header/Navbar';
+
+import '../styles/App.less';
 
 const App = () => {
   return (

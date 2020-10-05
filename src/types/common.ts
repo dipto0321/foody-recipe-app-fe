3a9ba51f17b44fd0af930b7971567b7503e96862
dipto0321 <diptokmk47@gmail.com>
2020-10-05 12:41:56 +0600

    :sparkles: Added menu selection indicator
    - Add menus as a state and then use menu selection configuration for ant-design

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,8 @@
 export type AccessDataObject = { refresh: string; access: string };
 export type HandleCommonFunc = () => void;
-export type HandleUpdateProfileWithArgsFunc = (data: ProfileDataObject) => void;
 export type ProfileDataObject = {
   name: string;
   email: string;
   avatarUrl: string;
 };
+export type HandleUpdateProfileWithArgsFunc = (data: ProfileDataObject) => void;

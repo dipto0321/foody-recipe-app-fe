07dbccebf4d79bfa0a7006ab806bd765cee03c47
dipto0321 <diptokmk47@gmail.com>
2020-10-24 19:17:54 +0600

    :sparkles: Redirection logic added for root path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
 
 const Routes = (): JSX.Element => {
   return (
     <Switch>
+      <Route exact path="/">
+        <Redirect to="/sign-in" />
+      </Route>
       <Route exact path="/sign-up" component={Signup} />
       <Route exact path="/sign-in" component={Signin} />
     </Switch>
